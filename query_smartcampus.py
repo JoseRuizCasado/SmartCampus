4fa7910e0bfef92c56e25ad72f7f53a8c7664d12
JoseRuizCasado <joseruizcasado02@gmail.com>
2021-04-23 20:07:28 +0200

    first commit

---
+++ query_smartcampus.py
@@ -0,0 +1,83 @@
+import requests
+import json
+import os
+import warnings
+from requests.packages.urllib3.exceptions import InsecureRequestWarning
+from time import sleep
+
+
+USER = "***"
+PASSWORD = "***"
+SERVICE = "***"
+SERVICE_PATH = "***"
+ORION_HOST = "***"
+
+warnings.simplefilter('ignore',InsecureRequestWarning)
+
+
+def get_token():
+    """Generate token to authenticate in Smart Campus Fiware service.
+
+    Returns:
+      token.
+
+    """
+  url = 'https://'+ORION_HOST+':6001/v3/auth/tokens'
+  headers = {'fiware-service': SERVICE, 'fiware-servicepath': SERVICE_PATH, 'Content-type': 'application/json'}
+
+  payload={
+    "auth": {
+        "identity": {
+            "methods": [
+                "password"
+            ],
+            "password": {
+                "user": {
+                    "domain": {
+                        "name": SERVICE
+                    },
+                    "name": USER,
+                    "password": PASSWORD
+                }
+            }
+        },
+        "scope": {
+            "project": {
+                "domain": {
+                   "name": SERVICE
+                },
+                "name": SERVICE_PATH
+            }
+        }
+    }
+}
+
+  response=requests.post(url, headers=headers, data=json.dumps(payload), verify=False)
+  response.encoding='utf-8'
+  print("Status code: ", response.status_code)
+  #print(response.text)
+  print(response.headers["X-Subject-Token"])
+  return response.headers["X-Subject-Token"]
+
+
+def get_data():
+    """Make request to Smart Campus Fiware service.
+
+    Returns:
+      Noise measured by the sensor.
+
+    """
+    token = get_token()
+    url = 'https://'+ORION_HOST+':2026/v2/entities?id=plugsense:PS02'
+    headers = {'fiware-service': SERVICE, 'fiware-servicepath': SERVICE_PATH, 'X-Auth-Token': token}  # Maybe the token changed
+    response = requests.get(url, headers=headers, verify=False)
+    response.encoding = 'utf-8'
+    #print(response.text)
+    response_dict=json.loads(response.text)
+    print("Status code: ", response.status_code)
+    # print(response.text)
+    noise = response_dict[0]['NOISE']['value']
+    datetime = response_dict[0]['NOISE']['metadata']['TimeInstant']['value']
+    print(noise, datetime)
+    return noise, datetime
+
---
+++ testing.py
@@ -0,0 +1,92 @@
+import plotly.graph_objects as go
+import pandas as pd
+import json
+import dash
+import dash_html_components as html
+import dash_core_components as dcc
+import dash_bootstrap_components as dbc
+from dash.dependencies import Input, Output, State
+from query_smartcampus import get_data
+
+
+df = pd.read_csv('data.csv')
+
+fig = go.Figure([go.Scatter(x=df['Date'], y=df['Noise'])])
+
+
+content = html.Div(
+    [
+        html.H1('Personal Stock Price Dashboard', 
+                style={'textAlign': 'center'}),
+        html.Hr(),
+        dbc.Row(
+            [
+                dbc.Col(dcc.Graph(id="graph", figure=fig),
+                        width={"size": 8, "offset": 2}),
+            ]
+        ),
+        dbc.Row(
+            [
+                dbc.Col(width={"size": 0.01, "offset": 2}),
+                dbc.Col(dbc.FormGroup([
+                            dbc.Button(
+                                id='dowload-button',
+                                n_clicks=0,
+                                children='Download',
+                                color="primary",
+                                block=True
+                            ),
+                        ]),
+                        width={"size": 4, "offset": 2}),
+                html.Div(id='intermediate-value', style={'display': 'none'})
+            ],
+            
+        ),
+        dcc.Interval(id='interval', interval=300000)
+    ]
+)
+
+app = dash.Dash(external_stylesheets=[dbc.themes.BOOTSTRAP])
+app.layout = html.Div([content])
+
+@app.callback(
+    Output('intermediate-value', 'children'),
+    [Input('dowload-button', 'n_clicks')])
+def show_input(n_clicks):
+    """"Save DataFrame data to csv file.
+
+    Args:
+      n_clicks: Number of clicks maked since app is running.
+
+    Returns:
+      Nothing, this event doesn't update fronted.
+
+    """
+    df.to_csv('data.csv')
+    return ''
+
+
+@app.callback(
+    Output('graph', 'figure'),
+    [Input('interval', 'n_intervals')]
+)
+def streamFig(value):
+    """"Update grapah periodically. Make request to UMA SmartCampus and append data
+    to global DataFrame
+    Args:
+      value: Number of intervals executed since app is running.
+
+    Returns:
+      The new figure object to update in front end.
+
+    """
+    global df
+    noise, timestamp = get_data()
+    new_data = {'Noise': noise, 'Date': timestamp}
+    df = df.append(new_data, ignore_index=True)
+    fig = go.Figure([go.Scatter(x=df['Date'], y=df['Noise'])])
+    return(fig)
+
+
+if __name__ == '__main__':
+    app.run_server(port='8083', debug=True)
